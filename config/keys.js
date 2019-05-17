dc2f7d56f14ee9fb83f089e522149ac30975a0d4
Jalsson <nilsson.niklas.97@gmail.com>
2019-05-17 14:35:18 +0300

    keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MongoURI: "mongodb+srv://Nilsson:XCwjlbPX2jEJykff@cluster1-9uf5p.mongodb.net/test?retryWrites=true"
+    MongoURI: "mongodb+srv://Nilsson:@cluster1-9uf5p.mongodb.net/test?retryWrites=true"
 }
 
